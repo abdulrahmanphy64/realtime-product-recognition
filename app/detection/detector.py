faee458e353292a2fa21914a9a5b5a1a10b91232
Abdul Rahman Shaikh <abdulrahmanphy64@gmail.com>
2025-11-30 21:43:36 +0530

    Add detector.py

---
+++ app/detection/detector.py
@@ -0,0 +1,12 @@
+class Detector:
+    def __init__(self,model_loader):
+        pass
+
+    def detect_image(self,image_path):
+        pass
+
+    def detect_video(self,video_path):
+        pass
+
+    def detect_webcam(self):
+        pass
